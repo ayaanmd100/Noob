fd7b91e84d6407c28a82d71fa906d56c16c02a16
ayaanmd100 <109349651+ayaanmd100@users.noreply.github.com>
2022-07-18 21:22:29 +0530

    Create Unfriender.js

---
+++ Unfriender.js
@@ -0,0 +1,10 @@
+
+const roblox = require(roblox.js)
+const userid = 1693395668
+const cookie = .ROBLOSECURITY.ENV
+
+roblox.UnfriendUserID(cookie, userid)
+if success then
+console.log("Success")
+else
+console.log("Failed")
